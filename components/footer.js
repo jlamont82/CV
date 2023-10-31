6dbd653e665a29de6376aa872e4600a1f88b0ad8
jlamont82 <57664420+jlamont82@users.noreply.github.com>
2023-10-31 12:01:23 +0000

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -18,7 +18,7 @@ footerTemplate.innerHTML = `
         <a class="footer-reference" href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
         <br>
       </p>
-      <p class="copyright">© <script>
+      <p class="copyright">&copy; <script>
 document.getElementById("year").innerHTML = new Date().getFullYear();</script> John Lamont.</p>
     </div>
   </footer>
