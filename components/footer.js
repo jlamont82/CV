98aedb32abde4e2a79d71a5ec482f7e62b1d963c
jlamont82 <57664420+jlamont82@users.noreply.github.com>
2024-04-26 12:04:32 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -21,7 +21,27 @@ footerTemplate.innerHTML = `
         <br>
       </p>
       
-      <p class="copyright">&copy; 2024 <script>document.write(/\d{4}/.exec(Date())[0])</script> John Lamont. </p> 
+      <p class="copyright">&copy; 2018 - <script>document.write(/\d{4}/.exec(Date())[0])</script> John Lamont. </p> 
+
+
+     <p> <script>
+      // create a function to update the date and time
+      function updateDateTime() {
+        // create a new `Date` object
+        const now = new Date();
+
+        // get the current date and time as a string
+        const currentDateTime = now.toLocaleString();
+
+        // update the `textContent` property of the `span` element with the `id` of `datetime`
+        document.querySelector('#datetime').textContent = currentDateTime;
+      }
+
+      // call the `updateDateTime` function every second
+      setInterval(updateDateTime, 1000);
+    </script>
+    </P>
+    
     </div>
   </footer>
 `;
