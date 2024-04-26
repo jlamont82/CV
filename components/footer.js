088f1bd60f120b74c167ca6b3eb86ee4f57fa46e
jlamont82 <57664420+jlamont82@users.noreply.github.com>
2024-04-26 09:04:08 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -21,8 +21,7 @@ footerTemplate.innerHTML = `
         <br>
       </p>
       
-      <p class="copyright">&copy; <script>document.write(/\d{4}/.exec(Date())[0])</script> John Lamont 2024. </p> 
-      <p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Your Name All Rights Reserved</p>
+      <p class="copyright">&copy; 2024 <script>document.write(/\d{4}/.exec(Date())[0])</script> John Lamont. </p> 
     </div>
   </footer>
 `;
